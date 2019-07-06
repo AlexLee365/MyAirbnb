73aa26df317567500c31c0d9d81bf92884f4a0ac
Alex <kjj9647@gmail.com>
2019-07-06 15:39:16 +0900

    MainViewController created

---
+++ MyAirbnb/MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  MyAirbnb
+//
+//  Created by 행복한 개발자 on 06/07/2019.
+//  Copyright © 2019 Alex Lee. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
