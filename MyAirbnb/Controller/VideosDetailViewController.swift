65c0bee8c937868a1c1a0e09e8c98e882738ecec
doyeongkim <doyeong.kim1219@gmail.com>
2019-07-16 10:50:07 +0900

    video added

---
+++ MyAirbnb/Controller/VideosDetailViewController.swift
@@ -0,0 +1,126 @@
+//
+//  VideosDetailViewController.swift
+//  MyAirbnb
+//
+//  Created by Solji Kim on 15/07/2019.
+//  Copyright © 2019 Alex Lee. All rights reserved.
+//
+
+import UIKit
+
+class VideosDetailViewController: UIViewController {
+    
+    let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.contentInsetAdjustmentBehavior = .never
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 100
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    let topView = TableviewTopView()
+    let bottomView = BottomView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configure()
+        setAutolayout()
+    }
+    
+    var isStatusBarWhite = true
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        if isStatusBarWhite {
+            return .lightContent
+        } else {
+            return .default
+        }
+    }
+    
+    private func configure() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        view.addSubview(tableView)
+        
+        view.addSubview(topView)
+        view.addSubview(bottomView)
+    }
+    
+    private func setAutolayout() {
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: bottomView.topAnchor).isActive = true
+        
+        topView.translatesAutoresizingMaskIntoConstraints = false
+        topView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        topView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        topView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        topView.heightAnchor.constraint(equalToConstant: 75).isActive = true
+        
+        bottomView.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension VideosDetailViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 15
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+}
+
+// MARK: - UITableViewDelegate
+
+extension VideosDetailViewController: UITableViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        guard let cell = tableView.cellForRow(at: IndexPath(row: 0, section: 0)) else { return }
+        
+        let becomeWhiteEndPoint = cell.frame.height - topView.frame.height
+        let becomeWhiteStartPoint = becomeWhiteEndPoint - 70
+        
+        let opacity = ( scrollView.contentOffset.y - becomeWhiteStartPoint ) / (becomeWhiteEndPoint - becomeWhiteStartPoint)
+        
+        if scrollView.contentOffset.y > becomeWhiteStartPoint {
+            navigationController?.view.backgroundColor = UIColor.white.withAlphaComponent(opacity)
+            topView.backgroundColor = UIColor.white.withAlphaComponent(opacity)
+            topView.backButton.setImage(UIImage(named: "backBlack"), for: .normal)
+            topView.heartButton.setImage(UIImage(named: "heartBlack"), for: .normal)
+            topView.shareButton.setImage(UIImage(named: "shareBlack"), for: .normal)
+            
+            isStatusBarWhite = false
+        } else {
+            navigationController?.view.backgroundColor = UIColor.white.withAlphaComponent(opacity)
+            topView.backgroundColor = UIColor.white.withAlphaComponent(opacity)
+            topView.backButton.setImage(UIImage(named: "backWhite"), for: .normal)
+            topView.heartButton.setImage(UIImage(named: "heartWhite"), for: .normal)
+            topView.shareButton.setImage(UIImage(named: "shareWhite"), for: .normal)
+            isStatusBarWhite = true
+        }
+        
+        let cellHeight = cell.frame.height
+        let currentY = scrollView.contentOffset.y
+        let deviceHeight = UIScreen.main.bounds.height
+        let bottomViewHeight = bottomView.frame.height
+        
+        if (cellHeight - currentY) <= (deviceHeight - bottomViewHeight) {
+            UIView.animate(withDuration: 0.3) {
+                self.bottomView.backgroundColor = .white
+                self.bottomView.makeShadow()
+            }
+        } else {
+            self.bottomView.backgroundColor = .black
+        }
+        
+        setNeedsStatusBarAppearanceUpdate()
+    }
+}
+
---
+++ MyAirbnb/View/Trip/LocalHosting/VideosDetailTableCell.swift
@@ -0,0 +1,30 @@
+//
+//  VideosDetailTableCell.swift
+//  MyAirbnb
+//
+//  Created by Solji Kim on 15/07/2019.
+//  Copyright © 2019 Alex Lee. All rights reserved.
+//
+
+import UIKit
+
+class VideosDetailTableCell: UITableViewCell {
+    static let identifier = "VideosDetailTableCell"
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        
+    }
+    
+    private func setAutolayout() {
+        
+    }
+    
+}
