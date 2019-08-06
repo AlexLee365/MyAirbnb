943f71b8a05f7d239b1915ad1b11960225b322ba
Alex <kjj9647@gmail.com>
2019-08-06 20:04:21 +0900

    houseImage added

--- MyAirbnb/Controller/TabbarController.swift
+++ MyAirbnb/Controller/TabbarController.swift
@@ -34,7 +34,7 @@ class TabbarController: UITabBarController {
     
     private func setTabbarItems() {
         let mainVC = UINavigationController(rootViewController: MainViewController()) 
-        let saveListVC = TestSaveViewController()
+        let saveListVC = FavoriteHouseListViewController()
         let tripNaviController = TestSaveViewController()
         let messageVC = UINavigationController(rootViewController: MessageViewController()) 
         let profileVC = MyPageViewController()
